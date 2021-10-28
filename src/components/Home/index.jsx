d46e02ddd4efea2a2c810ac9da131033798c6f82
bregarulai <bregarulai@gmail.com>
2021-10-28 16:07:02 -0400

    created store and movies slice

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,10 +3,13 @@ import { useEffect } from "react";
 
 import { MovieListing } from "../../components";
 import movieApi from "../../common/api/movieApi";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const Home = () => {
+  const dispatch = useDispatch();
   const movieText = "Harry";
   const fetchMovies = async () => {
     const response = await movieApi
@@ -14,6 +17,7 @@ const Home = () => {
       .catch((err) => {
         console.log("ERROR: ", err);
       });
+    dispatch(addMovies(response.data));
     console.log("RESPONSE: ", response);
   };
 
